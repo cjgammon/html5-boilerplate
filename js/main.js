0901f7b996adba4b85c94a8ee81debe01e7df43f
cjgammon <cj.gammon@gmail.com>
2013-01-20 23:25:40 -0800

    incorporate requirejs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+require.config({
+  shim: {
+  },
 
+  paths: {
+    jquery: 'vendor/jquery-1.9.0.min',
+    signals: 'vendor/signals.min'
+  }
+});
+ 
+require(['signals'], function() {
+    
+});
